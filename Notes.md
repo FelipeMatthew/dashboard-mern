822554809b71b73fecbe91e7b85754e7dcb49bfa
FelipeMatthew <felipematthewnascimento@gmail.com>
2023-02-22 23:23:38 -0300

    commiting

--- Notes.md
+++ Notes.md
@@ -1,2 +1,7 @@
 # Dashboard project Mern
 This file was raised to write all my notes
+
+ - CSSbaseline just do the css default props
+ - Study about the material ui documentation
+ 
+ -Browser Router - reactrouter dom
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,35 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { themeSettings } from "./config/theme";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Dashboard from "scenes/dashboard";
+
 export default function App() {
+  // Vai pegar o redux do estado
+  const mode = useSelector((state) => state.global.mode);
+
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]);
+
+// Setting up MATERIAL UI MUI = use the light and the dark mode
+
+// vai usar em todas as telas o layout
+// Element = Always pages will have this element called layout.
   return (
     <div className="app">
-      teste push
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" repĺace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
